d250ecaa77edcbd4cbe7fa227cf265400fbc7194
ukubach <ukubach.making@gmx.de>
2024-12-10 11:25:44 +0100

    Correct the tilting of the x-axis description.

--- src/models/foreign_beer.py
+++ src/models/foreign_beer.py
@@ -319,7 +319,8 @@ def plot_bar_chart(df_plot, save=True):
     )
 
     fig.update_yaxes(title_text="Average rating", type="log")
-    fig.update_layout(title_text="Average Rating Per Country (Bar Chart)")
+    fig.update_layout(title_text="Average Rating Per Country (Bar Chart)",
+                      xaxis=dict(tickangle=90))
 
     if save:
         fig.write_html("src/plots/bar_chart_avg_rating.html")
