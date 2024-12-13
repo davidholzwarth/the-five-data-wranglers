243721d1ad9418dfd938eadc9acc7824668e1dbd
ukubach <ukubach.making@gmx.de>
2024-12-13 11:47:11 +0100

    Trying to change color and layout of the bar chart for average rating per country.

--- src/models/foreign_beer.py
+++ src/models/foreign_beer.py
@@ -308,12 +308,13 @@ def plot_bar_chart(df_plot, save=True):
     df_plot.reset_index(drop=True, inplace=True)
     df_plot.sort_values(ascending=False, by="rating", inplace=True)
 
+    # Use the first color from the CB_color_cycle list
     fig = go.Figure(
         data=[
             go.Bar(
                 x=df_plot["location"],
                 y=df_plot["rating"],
-                marker=dict(color=colors * (len(df_plot) + 1 // len(colors)))
+                marker=dict(color=CB_color_cycle[0])  # First color from CB_color_cycle
             )
         ]
     )
